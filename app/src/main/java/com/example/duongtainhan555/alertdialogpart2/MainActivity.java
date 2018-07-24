7f3d6c227e3719c7b63233a7382aaa1fb667b65c
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-07-25 00:42:08 +0700

    Khởi tạo + Ánh xạ

--- app/src/main/java/com/example/duongtainhan555/alertdialogpart2/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/alertdialogpart2/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.duongtainhan555.alertdialogpart2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView lvName;
+    ArrayList<String> arrayName;
+    ArrayAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Anh xa
+        lvName = findViewById(R.id.lvName);
+        arrayName = new ArrayList<>();
     }
 }
