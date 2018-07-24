d3b98e0e61fe7d3adb8066d48c5d3f8967b44f95
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-07-25 01:05:37 +0700

    Long click + Alert Dialog

--- app/src/main/java/com/example/duongtainhan555/alertdialogpart2/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/alertdialogpart2/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.duongtainhan555.alertdialogpart2;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -23,6 +27,14 @@ public class MainActivity extends AppCompatActivity {
         //
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayName);
         lvName.setAdapter(adapter);
+
+        lvName.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                XacNhanXoa(i);
+                return false;
+            }
+        });
     }
 
     private void AddArrayName(){
@@ -32,6 +44,34 @@ public class MainActivity extends AppCompatActivity {
         arrayName.add("ASP.NET");
         arrayName.add("UNITY 3D");
         arrayName.add("COCOS2DX");
-        arrayName.add("NODEJS");
+        arrayName.add("NODE JS");
+    }
+
+    private void XacNhanXoa(final int pos)
+    {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+        alertDialog.setTitle("Notification !!!");
+        alertDialog.setIcon(R.mipmap.ic_launcher);
+        alertDialog.setMessage("Do you want to delete this subject?");
+        alertDialog.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                arrayName.remove(pos);
+                adapter.notifyDataSetChanged();
+            }
+        });
+        alertDialog.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        alertDialog.setNeutralButton("CANCEL", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        alertDialog.show();
     }
 }
