1c2c8abb23704378fdabf0a218e6dbb1a699d922
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-07-25 01:45:47 +0700

    Alert Dialog (advanced)

--- app/src/main/java/com/example/duongtainhan555/alertdialogpart2/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/alertdialogpart2/MainActivity.java
@@ -7,13 +7,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private ListView lvName;
+    private Button btnAdd;
+    private EditText edItem;
     ArrayList<String> arrayName;
     ArrayAdapter adapter;
     @Override
@@ -23,7 +28,9 @@ public class MainActivity extends AppCompatActivity {
         //Anh xa
         lvName = findViewById(R.id.lvName);
         arrayName = new ArrayList<>();
-        AddArrayName();
+        //Anh xa (advanced)
+        btnAdd = findViewById(R.id.btnAdd);
+        edItem = findViewById(R.id.edItem);
         //
         adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,arrayName);
         lvName.setAdapter(adapter);
@@ -35,16 +42,21 @@ public class MainActivity extends AppCompatActivity {
                 return false;
             }
         });
-    }
-
-    private void AddArrayName(){
-        arrayName.add("Android");
-        arrayName.add("IOS");
-        arrayName.add("PHP");
-        arrayName.add("ASP.NET");
-        arrayName.add("UNITY 3D");
-        arrayName.add("COCOS2DX");
-        arrayName.add("NODE JS");
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(edItem.getText().toString().equals(""))
+                {
+                    Toast.makeText(MainActivity.this,"NO DATA IN EDIT TEXT",Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    arrayName.add(edItem.getText().toString());
+                    adapter.notifyDataSetChanged();
+                    edItem.setText("");
+                }
+            }
+        });
     }
 
     private void XacNhanXoa(final int pos)
